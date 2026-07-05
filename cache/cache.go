c680e3e02fac8ca0820079c23aae8f5daf1f30fe
Blue-Onion <rawatadityasingh963@gmail.com>
2026-07-05 22:04:21 +0530

    func define

--- cache/cache.go
+++ cache/cache.go
@@ -1,9 +1,26 @@
 package cache
 
-import "github.com/google/uuid"
+import (
+	"sync"
+
+	"github.com/google/uuid"
+)
 
 type Cache struct {
+	mu     sync.RWMutex
 	Users  map[uuid.UUID]UserCache
 	Events map[uuid.UUID]EventCache
 	Arts   map[uuid.UUID]ArtCache
 }
+
+func GetUser(id uuid.UUID)            {}
+func DeleteUser(id uuid.UUID)         {}
+func SetUser(id uuid.UUID, user User) {}
+
+func GetArt(id uuid.UUID)          {}
+func DeleteArt(id uuid.UUID)       {}
+func SetArt(id uuid.UUID, art Art) {}
+
+func SetEvent(id uuid.UUID, event Event) {}
+func GetEvent(id uuid.UUID)              {}
+func DeleteEvent(id uuid.UUID)           {}
